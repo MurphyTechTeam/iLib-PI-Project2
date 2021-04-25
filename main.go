b93a4c38bc64e31100309bec5dd74317d9bbbe00
Muhammad Hilmi Ramadhan <hilmi.muhammad.rmdhn@gmail.com>
2021-04-25 08:06:41 +0700

    Penyambungan database dengan pckg gonfig & mysql

--- main.go
+++ main.go
@@ -1,10 +1,15 @@
 package main
 
-import "mhilmi999/project-2-mhilmi999/routes"
+import (
+	"mhilmi999/project-2-mhilmi999/db"
+	"mhilmi999/project-2-mhilmi999/routes"
+)
 
 
 
 func main(){
+	db.Init()
+	
 	e := routes.Init() 
 
 	e.Logger.Fatal(e.Start(":1234")) // Running proses web server di port http://localhost:1234
